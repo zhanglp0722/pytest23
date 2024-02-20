408edf3f28f34aa3273ad5f2d6ff35a2eef5e435
zhanglp0722 <81667149+zhanglp0722@users.noreply.github.com>
2024-02-20 11:45:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import os
-import pytest
+# import pytest
 
-def test_aa():
-    print("test")
+#def test_aa():
+    ##print("test")
 ​
 #步骤1： 通过Pytest生成测试报告数据文件，这里带有--alluredir参数 ，生成的数据文件目录为allure-result中
-pytest.main(['-s', '-v', '{test_aa}', '--alluredir', 'allure-results'])
+#pytest.main(['-s', '-v', '{test_aa}', '--alluredir', 'allure-results'])
 ​
 # 步骤2 ： 通过allure 读取数据源生成测试报告
 """
 解释：
 allure-result : 生成的json数据文件目录
 """
-cmd = "allure generate allure-results -c"
-os.system(cmd)
+#cmd = "allure generate allure-results -c"
+#os.system(cmd)
+print("这是输出内容")
